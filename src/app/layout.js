d7eeaf02feed430ddd6ed5c9d2d1cf56cd3bc4a4
Md Hridoy Sheikh <mdhridoy3240@gmail.com>
2024-12-09 07:30:07 +0600

    navbar complete without image

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/Components/Navbar";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+        <Navbar></Navbar>
         {children}
       </body>
     </html>
