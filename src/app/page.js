137cc175c08fdf9eda0299f806ffd68716c9927e
Md Hridoy Sheikh <mdhridoy3240@gmail.com>
2024-12-10 11:12:54 +0600

    scrolbar complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import Catagories from "@/components/Catagories";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/sliders";
 
@@ -7,11 +8,19 @@ export default function Home() {
     <div className="">
       <main className="">
 
-       <Slider></Slider>
-       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
-        <h1 className="text-2xl ">Featured Product</h1>
-        <ProductList></ProductList>
-       </div>
+        <Slider></Slider>
+        <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="text-2xl ">Featured Product</h1>
+          <ProductList></ProductList>
+        </div>
+        <div className="mt-24 ">
+          {/* <h1 className="text-2xl ">Featured Product</h1> */}
+          <Catagories></Catagories>
+        </div>
+        <div className="mt-24 px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
+          <h1 className="text-2xl ">Featured Product</h1>
+          <ProductList></ProductList>
+        </div>
 
       </main>
 
---
+++ src/components/Catagories.jsx
@@ -0,0 +1,57 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const Catagories = () => {
+    return (
+        <div className='px-4 overflow-x-scroll scroll-m-1 scrollbar-hide'>
+            <div className="flex gap-4 md:gap-8">
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+                <Link href="/list?cat=test" className='flex-shrink-0 w-full sm:w-1/2 l:1/4 xl:w-1/6'>
+                    <div className="relative bg-slate-100 w-full h-96">
+                        <Image src="https://i.ibb.co/09qWwfN/Razer-Death-Adder.jpg" fill alt='' sizes='20vh' className='object-cover'></Image>
+                    </div>
+                    <h1 className='mt-8 font-light text-xl tracking-wide'>Category Name</h1>
+                </Link>
+            </div>
+
+        </div>
+    );
+};
+
+export default Catagories;
